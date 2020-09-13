ad5ccdb4402e0f1becc2c6b4190417a7d986d599
aravcoding <69563665+aravcoding@users.noreply.github.com>
2020-09-13 16:35:25 -0500

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,15 @@
+class Bob{
+    constructor(x, y,radius){
+        var options = {
+            isStactic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body= Bodies.circle(x,y,radius);
+        World.add (world,this.body)
+    }
+
+
+
+}
